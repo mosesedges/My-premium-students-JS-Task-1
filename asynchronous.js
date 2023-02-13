390167c4fd4c6836db0028a81a1faab51801f38e
Moses  Ejele <mosesedges@gmail.com>
2023-02-13 18:21:22 +0000

    task on promise and fetch

--- asynchronous.js
+++ asynchronous.js
@@ -5,14 +5,52 @@ const options = {
 		'X-RapidAPI-Host': 'weatherapi-com.p.rapidapi.com'
 	}
 };
-async function london (handle) {
-const url = 'https://weatherapi-com.p.rapidapi.com/forecast.json?q=London&days=3'
-const response = await fetch(url, handle);
-const weatherLondon = await response.json();
-console.log(weatherLondon.location.name)
-console.log(weatherLondon.location.lat)
-console.log(weatherLondon.location.lon)
 
+const  london  = async (handle) => {
+const url = 'https://weatherapi-com.p.rapidapi.com/forecast.json?q=London&days=3'
+const response  = await fetch(url, handle)
+return await response.json()
 }
 
 london(options)
+.then(weather => console.log(weather.location.tz_id))
+
+// promise 
+const userData = new Promise((resolve, reject) => {
+	setTimeout(() =>{
+	const username = "moEdges"
+	if(username) {
+		resolve({name:"Moses", username, email:"example.com", avater:"rhino.png"})
+	}else {
+		reject("there is no data returned")
+	}	
+	},2000)
+})
+
+userData
+	.then(data => console.log(data))
+	.catch(err => console.log(err))
+
+
+//Fetch Get
+fetch('https://jsonplaceholder.typicode.com/comments/1')
+.then(response => response.json())
+.then(data => console.log(data))
+	
+
+// fetch POST
+
+fetch('https://jsonplaceholder.typicode.com/comments/',{
+	method: "POST",
+	body:JSON.stringify({
+		id: 1,
+		name: "Rango",
+		region:"Africa",
+		email: "thisaims@example.com",
+		body: "this is this request body"
+
+	})
+
+})
+.then(response => response.json())
+.then(data => console.log(data))	
