047f9d75cb3dd0d33e8d9af2358bcc66bd0e73a4
Moses  Ejele <mosesedges@gmail.com>
2023-02-14 00:00:56 +0000

    react added

---
+++ reactthing/index.js
@@ -0,0 +1,15 @@
+// ReactDOM.render(<h1>Hello, React!</h1>, document.getElementById("root"))
+
+/* 
+Challenge - recreate the above line of code in vanilla JS by creating and
+appending an h1 to our div#root (without using innerHTML).
+
+- Create a new h1 element
+- Give it some textContent
+- Give it a class name of "header"
+- append it as a child of the div#root
+    
+*/
+
+const message = document.getElementById("root")
+message.innerHTML = `<h1 class="header">I am the new kid</h1>`
